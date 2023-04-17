17b4a7a07db1bb4573ae023eb94549bd6eb4e3ed
Taeho <bueeciel9@gmail.com>
2023-04-17 06:37:15 -0500

    commit

--- 3d_Object_Detection/dada_backbone.py
+++ 3d_Object_Detection/dada_backbone.py
@@ -412,38 +412,51 @@ class DADA3d(Attention3d):
 
         return _gather_dict
 
-    def calculate_density(self, num_points, max_num_points_per_voxel):
-        density = num_points / max_num_points_per_voxel
-        return density
+    def calculate_density(self, num_points_per_voxel_dict, max_num_points_per_voxel):
+        num_points_per_voxel_tensor = torch.tensor(list(num_points_per_voxel_dict.values())).float()
+        density = num_points_per_voxel_tensor / max_num_points_per_voxel
+        return dict(zip(list(num_points_per_voxel_dict.keys()), density.tolist()))
 
-    def find_reference_and_adjacent_voxel_indices(self, voxel_centers, reference_voxels, voxel_size):
+
+    def find_reference_and_adjacent_voxel_indices(self, voxel_centers, reference_voxels, voxel_size, num_points, max_num_points_per_voxel):
         reference_indices = []
         adjacent_indices_list = []
         highest_density_indices_list = []
 
+        device = voxel_centers.device
+        density = self.calculate_density(num_points, max_num_points_per_voxel)
+
+
         for reference_voxel in reference_voxels:
-            reference_voxel_tensor = torch.tensor(reference_voxel, dtype=torch.float)
+            reference_voxel_tensor = reference_voxel.clone().detach().to(dtype=torch.float, device=device)
             distances = torch.abs(voxel_centers - reference_voxel_tensor)
-            reference_idx = np.where(np.all(distances.numpy() < 1e-6, axis=1, keepdims=True))[0]
+            reference_idx = torch.where(torch.all(distances < 1e-6, dim=1))[0]
 
             adjacent_indices = []
-            if reference_idx.size != 0:
-                reference_indices.append(reference_idx[0])
+            if reference_idx.numel() != 0:
+                reference_indices.append(reference_idx[0].item())
 
                 for i, center in enumerate(voxel_centers):
                     diff = torch.abs(center - reference_voxel_tensor)
-                    conditions = torch.logical_and(diff >= torch.tensor(voxel_size), diff <= torch.tensor(voxel_size) * 2)
+                    conditions = torch.logical_and(diff >= torch.tensor(voxel_size, device = device), diff <= torch.tensor(voxel_size, device = device) * 2)
                     if torch.all(conditions):
                         adjacent_indices.append(i)
 
                 adjacent_indices_list.append(adjacent_indices)
 
                 if adjacent_indices:
-                    highest_density_idx = np.argwhere(density == np.max(density[adjacent_indices])).flatten()
-                else:
-                    highest_density_idx = []
+                    adjacent_indices_tensor = torch.tensor(adjacent_indices, device=device)
+            
+                    # Check if all adjacent_indices_tensor are in the num_points_per_voxel_dict
+                    if all(idx.item() in num_points for idx in adjacent_indices_tensor):
+                        density_adjacent_indices = torch.tensor([num_points[idx.item()] for idx in adjacent_indices_tensor], device=device)
+                        highest_density_idx_tuple = torch.where(density_adjacent_indices == torch.max(density_adjacent_indices))
+                        highest_density_idx = highest_density_idx_tuple[0].flatten()
+                    else:
+                        highest_density_idx = torch.tensor([], device=device)
 
-                highest_density_indices_list.append(highest_density_idx)
+                    highest_density_indices_list.append(highest_density_idx)
+
 
         return reference_indices, adjacent_indices_list, highest_density_indices_list
 
@@ -468,17 +481,21 @@ class DADA3d(Attention3d):
         query_features = voxel_features.unsqueeze(0) # (1, N1+N2, C)
         key_features = votr_utils.grouping_operation(voxel_features, v_bs_cnt, key_indices, k_bs_cnt)
 
+        unique_indices, num_points_per_voxel = torch.unique(sp_tensor.indices[:, 3], return_counts=True)
 
+        # Create a dictionary to map unique_indices to num_points_per_voxel
+        num_points_per_voxel_dict = dict(zip(unique_indices.tolist(), num_points_per_voxel.tolist()))
 
         # Calculate density
-        num_points = sp_tensor.indices.size(0)
+        # num_points = sp_tensor.indices.size(0)
         max_num_points_per_voxel = sp_tensor.features.size(1)
-        density = self.calculate_density(num_points, max_num_points_per_voxel)
 
         # Find reference and adjacent voxel indices
         voxel_centers = self.with_coords(sp_tensor.indices, sp_tensor.point_cloud_range, sp_tensor.voxel_size)
-        reference_voxels = voxel_centers.clone()
-        reference_indices, adjacent_indices_list, highest_density_indices_list = self.find_reference_and_adjacent_voxel_indices(voxel_centers, reference_voxels, sp_tensor.voxel_size)
+        num_random_voxels = 100
+        random_voxel_indices = torch.randperm(voxel_centers.shape[0])[:num_random_voxels]
+        reference_voxels = voxel_centers[random_voxel_indices]
+        reference_indices, adjacent_indices_list, highest_density_indices_list = self.find_reference_and_adjacent_voxel_indices(voxel_centers, reference_voxels, sp_tensor.voxel_size, num_points_per_voxel_dict, max_num_points_per_voxel )
 
         # Move reference voxel's position to the highest density voxel's position
         for idx, highest_density_indices in zip(reference_indices, highest_density_indices_list):
@@ -537,7 +554,7 @@ class DADAResBlock(nn.Module):
     #     # Don't forget to combine local attention and Density-Aware Deformable Attention as needed
     #     return super().forward(sp_tensor)
     def __init__(self, model_cfg, use_relative_coords = False, use_pooled_feature = False, use_no_query_coords = False):
-        super(AttentionResBlock, self).__init__()
+        super(DADAResBlock, self).__init__()
         sp_cfg = model_cfg.SP_CFGS
         self.sp_attention = SparseAttention3d(
             input_channels = sp_cfg.CHANNELS[0],
@@ -552,10 +569,26 @@ class DADAResBlock(nn.Module):
             use_pooled_feature = use_pooled_feature,
             use_no_query_coords= use_no_query_coords,
         )
+        # Original.
+        # subm_cfg = model_cfg.SUBM_CFGS
+        # self.subm_attention_modules = nn.ModuleList()
+        # for i in range(subm_cfg.NUM_BLOCKS):
+        #     self.subm_attention_modules.append(SubMAttention3d(
+        #         input_channels = subm_cfg.CHANNELS[0],
+        #         output_channels = subm_cfg.CHANNELS[2],
+        #         ff_channels = subm_cfg.CHANNELS[1],
+        #         dropout = subm_cfg.DROPOUT,
+        #         num_heads = subm_cfg.NUM_HEADS,
+        #         attention_modes = subm_cfg.ATTENTION,
+        #         use_pos_emb =  subm_cfg.USE_POS_EMB,
+        #         use_relative_coords = use_relative_coords,
+        #         use_no_query_coords= use_no_query_coords,
+        #     ))
+        # DADA.
         subm_cfg = model_cfg.SUBM_CFGS
         self.subm_attention_modules = nn.ModuleList()
         for i in range(subm_cfg.NUM_BLOCKS):
-            self.subm_attention_modules.append(SubMAttention3d(
+            self.subm_attention_modules.append(DADA3d(
                 input_channels = subm_cfg.CHANNELS[0],
                 output_channels = subm_cfg.CHANNELS[2],
                 ff_channels = subm_cfg.CHANNELS[1],
@@ -608,40 +641,7 @@ class DADATransformer(nn.Module):
     #     num_points = num_points.numpy()
     #     return voxel_centers, num_points
 
-    def calculate_density(self, num_points, max_num_points_per_voxel):
-        density = num_points / max_num_points_per_voxel
-        return density
 
-    def find_reference_and_adjacent_voxel_indices(self, voxel_centers, reference_voxels, voxel_size):
-        reference_indices = []
-        adjacent_indices_list = []
-        highest_density_indices_list = []
-
-        for reference_voxel in reference_voxels:
-            reference_voxel_tensor = torch.tensor(reference_voxel, dtype=torch.float)
-            distances = torch.abs(voxel_centers - reference_voxel_tensor)
-            reference_idx = np.where(np.all(distances.numpy() < 1e-6, axis=1, keepdims=True))[0]
-
-            adjacent_indices = []
-            if reference_idx.size != 0:
-                reference_indices.append(reference_idx[0])
-
-                for i, center in enumerate(voxel_centers):
-                    diff = torch.abs(center - reference_voxel_tensor)
-                    conditions = torch.logical_and(diff >= torch.tensor(voxel_size), diff <= torch.tensor(voxel_size) * 2)
-                    if torch.all(conditions):
-                        adjacent_indices.append(i)
-
-                adjacent_indices_list.append(adjacent_indices)
-
-                if adjacent_indices:
-                    highest_density_idx = np.argwhere(density == np.max(density[adjacent_indices])).flatten()
-                else:
-                    highest_density_idx = []
-
-                highest_density_indices_list.append(highest_density_idx)
-
-        return reference_indices, adjacent_indices_list, highest_density_indices_list
 
     def forward(self, batch_dict):
         voxel_features, voxel_coords = batch_dict['voxel_features'], batch_dict['voxel_coords']
